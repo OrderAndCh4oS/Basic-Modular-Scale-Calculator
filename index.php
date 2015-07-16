a1bf051e83faa0747e3c12499d3497e14478af62
Sarcoma <sarcoma@live.co.uk>
2015-07-16 10:19:13 +0100

    Made calculations more accurate by only rounding at end of calculations

--- index.php
+++ index.php
@@ -1,117 +1,137 @@
-<?php require_once('mod-scale-calc.php'); ?>
+<?php require_once( 'mod-scale-calc.php' ); ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Modular Scale Calculator</title>
-        <meta name="description" content="A modular scale calculator for working out double stranded scales to aid typographic rhythm.">
-        <meta name="viewport" content="width=960px">
-
-        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <link rel="icon" href="favicon.ico">
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-        <script type="text/javascript" src="http://fast.fonts.net/jsapi/d200305d-8416-404a-a388-46e78638340d.js"></script>
-        <?php include_once('google-analytics.php'); ?>
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-
-        <!-- Add your site or application content here -->
-        <article class="wrapper">
-            <h1>Modular Scale Calculator</h1>
-            <p class="large">A double stranded modular scale calculator for working out typographic rhythm.</p>
-            <form method="post" action="http://modular-scale.com/">
-                <fieldset>
-                    <legend>Calculator</legend>
-                    <p class="input">
-                        <label for="base">Figure</label>
-                        <input id="base" name="base" type="text" value="<?php echo $base; ?>">
-                        <?php if(!empty($error['base'])) { echo "<span class='error-message'>{$error['base']}</span>"; } ?>
-                    </p>
-                    <p class="input">
-                        <label for="alt">Important Figure</label>
-                        <input id="alt" name="alt" type="text" value="<?php echo $alt; ?>">
-                        <?php if(!empty($error['alt'])) { echo "<span class='error-message'>{$error['alt']}</span>"; } ?>
-                    </p>
-                    <p>
-                        <label for="ratio">Ratio</label>
-                        <select id="ratio" name="ratio">
-                            <option value="1.067" <?php if($ratio == 1.067) echo "selected" ?>>15:16&#8201;&ndash;&#8201;minor second</option>
-                            <option value="1.125" <?php if($ratio == 1.125) echo "selected" ?>>8:9&#8201;&ndash;&#8201;major second</option>
-                            <option value="1.2" <?php if($ratio == 1.2) echo "selected" ?>>5:6&#8201;&ndash;&#8201;minor third</option>
-                            <option value="1.25" <?php if($ratio == 1.25) echo "selected" ?>>4:5&#8201;&ndash;&#8201;major third</option>
-                            <option value="1.333" <?php if($ratio == 1.333) echo "selected" ?>>3:4&#8201;&ndash;&#8201;perfect fourth</option>
-                            <option value="1.414" <?php if($ratio == 1.414) echo "selected" ?>>1:&radic;2&#8201;&ndash;&#8201;aug. fourth / dim. fifth</option>
-                            <option value="1.5" <?php if($ratio == 1.5) echo "selected" ?>>2:3&#8201;&ndash;&#8201;perfect fifth</option>
-                            <option value="1.6" <?php if($ratio == 1.6) echo "selected" ?>>5:8&#8201;&ndash;&#8201;minor sixth</option>
-                            <option value="1.618" <?php if($ratio == 1.618 || !isset($scale)) echo "selected" ?>>1:1.618&#8201;&ndash;&#8201;golden section</option>
-                            <option value="1.667" <?php if($ratio == 1.667) echo "selected" ?>>3:5&#8201;&ndash;&#8201;major sixth</option>
-                            <option value="1.778" <?php if($ratio == 1.778) echo "selected" ?>>9:16&#8201;&ndash;&#8201;minor seventh</option>
-                            <option value="1.875" <?php if($ratio == 1.875) echo "selected" ?>>8:15&#8201;&ndash;&#8201;major seventh</option>
-                        </select>
-                    </p>
-                    <p class="submit"><input type="submit" name="submit" value="Submit"></p>
-                </fieldset>
-            </form>
-            <?php if(isset($scale)) : ?>
-            <table>
-                <tr>
-                    <th class="col">Base</th>
-                    <th class="col">Alt</th>
-                    <th class="col">Em Base</th>
-                    <th class="col">Em Alt</th>
-                    <th class="col">@16 Base</th>
-                    <th class="col">@16 Alt</th>
-                </tr>
-                <tr>
-                    <?php
-                            foreach($scale as $row) {
-                                $output = '<tr>';
-                                $output .= scaleRow($row[0], $basePoints);
-                                $output .= scaleRow($row[1], $altPoints);
-                                $output .= scaleRow($row[0], $basePoints, 'ems');
-                                $output .= scaleRow($row[1], $altPoints, 'ems', $basePoints);
-                                $output .= scaleRow($row[0], $basePoints, 'ems', 16);
-                                $output .= scaleRow($row[1], $altPoints, 'ems', 16);
-                                $output .= '</tr>';
-                                echo $output;
-
-                            }
-
-                    ?>
-                </tr>
-            </table>
-            <?php endif; ?>
-            <footer class="clearfix">
-                <div>
-                    <h2>Built by</h2>
-                    <ul>
-                        <li><a href="https://twitter.com/Sarkoma">@Sarkoma</a></li>
-                        <li><a href="https://twitter.com/Sarkoma">@threeandme_</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>Github</h2>
-                        <p><a href="https://github.com/sarcoma/Basic-Modular-Scale-Calculator">Modular Scale Calculator</a></p>
-                </div>
-                <div>
-                    <h2>Further Resources</h2>
-                    <ul>
-                        <li><a href="http://alistapart.com/article/more-meaningful-typography">More Meaningful Typography</a></li>
-                        <li><a href="http://alistapart.com/article/content-out-layout">Content Out Layout</a></li>
-                        <li><a href="http://www.goodreads.com/book/show/44735.The_Elements_of_Typographic_Style">The Elements of Typographic Style</a></li>
-                    </ul>
-                </div>
-            </footer>
-        </article>
-    </body>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Modular Scale Calculator</title>
+    <meta name="description" content="A basic modular scale calculator for working out typographic rhythm. Designed and developed by Sean Cooper.">
+    <meta name="viewport" content="width=960px">
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    <script type="text/javascript" src="http://fast.fonts.net/jsapi/d200305d-8416-404a-a388-46e78638340d.js"></script>
+    <?php include_once( 'google-analytics.php' ); ?>
+</head>
+<body>
+<!--[if lt IE 7]><p class="browsehappy">You are using an <strong>outdated</strong> browser. Please
+    <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.
+</p><![endif]--><!-- Add your site or application content here -->
+<article class="wrapper">
+    <h1>Modular Scale Calculator</h1>
+    <p class="large">A double stranded modular scale calculator for working out typographic rhythm.</p>
+    <form method="post" action="index.php">
+        <fieldset>
+            <legend>Calculator</legend>
+            <p><label for="base">Figure</label> <input id="base" name="base" type="text" value="<?php echo $base; ?>">
+            </p>
+            <p><label for="alt">Important Figure</label>
+                <input id="alt" name="alt" type="text" value="<?php echo $alt; ?>"></p>
+            <p>
+                <label for="ratio">Ratio</label> <select id="ratio" name="ratio">
+                    <option value="1.067" <?php if ($ratio == 1.067)
+                        echo "selected" ?>>15:16&#8201;&ndash;&#8201;minor second
+                    </option>
+                    <option value="1.125" <?php if ($ratio == 1.125)
+                        echo "selected" ?>>8:9&#8201;&ndash;&#8201;major second
+                    </option>
+                    <option value="1.2" <?php if ($ratio == 1.2)
+                        echo "selected" ?>>5:6&#8201;&ndash;&#8201;minor third
+                    </option>
+                    <option value="1.25" <?php if ($ratio == 1.25)
+                        echo "selected" ?>>4:5&#8201;&ndash;&#8201;major third
+                    </option>
+                    <option value="1.333" <?php if ($ratio == 1.333)
+                        echo "selected" ?>>3:4&#8201;&ndash;&#8201;perfect fourth
+                    </option>
+                    <option value="1.414" <?php if ($ratio == 1.414)
+                        echo "selected" ?>>1:&radic;2&#8201;&ndash;&#8201;aug. fourth / dim. fifth
+                    </option>
+                    <option value="1.5" <?php if ($ratio == 1.5)
+                        echo "selected" ?>>2:3&#8201;&ndash;&#8201;perfect fifth
+                    </option>
+                    <option value="1.6" <?php if ($ratio == 1.6)
+                        echo "selected" ?>>5:8&#8201;&ndash;&#8201;minor sixth
+                    </option>
+                    <option value="1.618" <?php if ($ratio == 1.618 || !isset( $scale ))
+                        echo "selected" ?>>1:1.618&#8201;&ndash;&#8201;golden section
+                    </option>
+                    <option value="1.667" <?php if ($ratio == 1.667)
+                        echo "selected" ?>>3:5&#8201;&ndash;&#8201;major sixth
+                    </option>
+                    <option value="1.778" <?php if ($ratio == 1.778)
+                        echo "selected" ?>>9:16&#8201;&ndash;&#8201;minor seventh
+                    </option>
+                    <option value="1.875" <?php if ($ratio == 1.875)
+                        echo "selected" ?>>8:15&#8201;&ndash;&#8201;major seventh
+                    </option>
+                </select>
+            </p>
+            <p class="submit"><input type="submit" name="submit" value="Submit"></p>
+        </fieldset>
+    </form>
+    <table>
+        <tr>
+            <th class="col">Base</th>
+            <th class="col">Alt</th>
+            <th class="col">Em Base</th>
+            <th class="col">Em Alt</th>
+            <th class="col">@16 Base</th>
+            <th class="col">@16 Alt</th>
+        </tr>
+        <tr>
+            <?php
+            if (isset( $scale )) {
+                foreach ($scale as $row) {
+                    $output = '<tr>';
+                    $output .= scaleRow($row[0], $basePoints);
+                    $output .= scaleRow($row[1], $altPoints);
+                    $output .= scaleRow($row[0], $basePoints, 'ems');
+                    $output .= scaleRow($row[1], $altPoints, 'ems', $basePoints);
+                    $output .= scaleRow($row[0], $basePoints, 'ems', 16);
+                    $output .= scaleRow($row[1], $altPoints, 'ems', 16);
+                    $output .= '</tr>';
+                    echo $output;
+                }
+            }
+            ?>
+        </tr>
+    </table>
+    <footer class="clearfix">
+        <div>
+            <h2>Built by</h2>
+            <ul>
+                <li><a href="https://twitter.com/Sarkoma">@Sarkoma</a></li>
+                <li><a href="https://twitter.com/Sarkoma">@threeandme_</a></li>
+            </ul>
+        </div>
+        <div>
+            <h2>Github</h2>
+            <p><a href="https://github.com/sarcoma/Basic-Modular-Scale-Calculator">Modular Scale Calculator</a></p>
+        </div>
+        <div>
+            <h2>Further Resources</h2>
+            <ul>
+                <li><a href="http://alistapart.com/article/more-meaningful-typography">More Meaningful Typography</a>
+                </li>
+                <li><a href="http://alistapart.com/article/content-out-layout">Content Out Layout</a></li>
+                <li>
+                    <a href="http://www.goodreads.com/book/show/44735.The_Elements_of_Typographic_Style">The Elements of Typographic Style</a>
+                </li>
+                <li><a href="http://www.guarnier.com/modularscale.html">Visual Examples of Modular Scales</a></li>
+            </ul>
+        </div>
+    </footer>
+</article>
+</body>
 </html>
--- mod-scale-calc.php
+++ mod-scale-calc.php
@@ -1,139 +1,146 @@
 <?php
 
-	function validate( $data ) {
-		$data = trim( $data );
-		$data = stripslashes( $data );
-		$data = htmlspecialchars( $data );
+function validate($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
 
-		return $data;
-	}
+    return $data;
+}
 
-	function isPica( $data ) {
-		if ( preg_match( '/^[0-9]+(?i)p([0-1]?[0-9])?$/i', $data ) ) {
-			return true;
-		}
+function isPica($data)
+{
+    if (preg_match('/^[0-9]+(?i)p([0-1]?[0-9])?$/i', $data)) {
+        return true;
+    }
 
-		return false;
-	}
+    return false;
+}
 
-	function isPoint( $data ) {
-		if ( preg_match( '/^[0-9]+(?i)pt$/i', $data ) ) {
-			return true;
-		}
+function isPoint($data)
+{
+    if (preg_match('/^[0-9]+(?i)pt$/i', $data)) {
+        return true;
+    }
 
-		return false;
-	}
+    return false;
+}
 
-	function makePoints( $data ) {
-		$data   = preg_replace( '/^([0-9]+)((?i)p)(([0-1]?[0-9])?)$/i', '$1.$3', $data );
-		$data   = explode( '.', $data );
-		$points = floor( $data[0] * 12 );
-		$points = $data[1] + $points;
+function makePoints($data)
+{
+    $data   = preg_replace('/^([0-9]+)((?i)p)(([0-1]?[0-9])?)$/i', '$1.$3', $data);
+    $data   = explode('.', $data);
+    $points = floor($data[0] * 12);
+    $points = $data[1] + $points;
 
-		return $points;
-	}
+    return $points;
+}
 
-	function scaleRow( $data, $points, $conversion = false, $emBase = false ) {
+function scaleRow($data, $points, $conversion = false, $emBase = false)
+{
 
-		$output = '<td class="col"><span';
-		if ( $data == $points ) {
-			$output .= ' class="highlight" ';
-		}
+    $output = '<td class="col"><span';
+    if ($data == $points) {
+        $output .= ' class="highlight" ';
+    }
 
-		if ( $conversion == 'ems' ) {
-			if ( ! $emBase ) {
-				$data = round( $data / $points, 3 );
-			} else {
-				$data = round( $data / $emBase, 3 );
-			}
-		}
+    if ($conversion == 'ems') {
+        if (!$emBase) {
+            $data = round($data / $points, 3);
+        } else {
+            $data = round($data / $emBase, 3);
+        }
+    } else {
+        $data = round($data,3);
+    }
 
-		$output .= '>' . $data . '</span></td>';
+    $output .= '>' . $data . '</span></td>';
 
-		return $output;
-	}
+    return $output;
+}
 
-	/* todo: Make sure entry is a valid number, maybe test for px, pt, p, ems etc and handle accordingly. */
-	/* todo: Make calculations for ems and percent. */
-	if ( isset( $_POST['submit'] ) ) {
-		$base  = validate( $_POST['base'] );
-		$alt   = validate( $_POST['alt'] );
-		$ratio = validate( $_POST['ratio'] );
+/* todo: Make sure entry is a valid number, maybe test for px, pt, p, ems etc and handle accordingly. */
+/* todo: Make calculations for ems and percent. */
+if (isset( $_POST['submit'] )) {
+    $base  = validate($_POST['base']);
+    $alt   = validate($_POST['alt']);
+    $ratio = validate($_POST['ratio']);
 
-		$error = array();
+    $error = array();
 
-		if ($base < 999999) {
-			if ( isPica( $base ) ) {
-				$basePoints = makePoints( $base );
-			} elseif ( isPoint( $base ) ) {
-				$basePoints = $base;
-			} elseif ( is_numeric( $base ) ) {
-				$basePoints = $base;
-			} else {
-				$basePoints = 0;
-				$error['base'] = "Data not valid";
-			}
-		} else {
-			$error['base'] = "Number too large";
-		}
+    if ($base < 999999) {
+        if (isPica($base)) {
+            $basePoints = makePoints($base);
+        } elseif (isPoint($base)) {
+            $basePoints = $base;
+        } elseif (is_numeric($base)) {
+            $basePoints = $base;
+        } else {
+            $basePoints    = 0;
+            $error['base'] = "Data not valid";
+        }
+    } else {
+        $error['base'] = "Number too large";
+    }
 
-		if ($alt < 999999) {
-			if ( isPica( $alt ) ) {
-				$altPoints = makePoints( $alt );
-			} elseif ( isPoint( $alt ) ) {
-				$altPoints = $alt;
-			} elseif ( is_numeric( $alt ) ) {
-				$altPoints = $alt;
-			} else {
-				$altPoints    = 0;
-				$error['alt'] = "Data not valid";
-			}
-		} else {
-			$error['alt'] = "Number too large";
-		}
-	} else {
-		$ratio      = 1.618;
-		$basePoints = 11;
-		$altPoints  = 210;
-		$base       = $basePoints;
-		$alt        = $altPoints;
-	}
+    if ($alt < 999999) {
+        if (isPica($alt)) {
+            $altPoints = makePoints($alt);
+        } elseif (isPoint($alt)) {
+            $altPoints = $alt;
+        } elseif (is_numeric($alt)) {
+            $altPoints = $alt;
+        } else {
+            $altPoints    = 0;
+            $error['alt'] = "Data not valid";
+        }
+    } else {
+        $error['alt'] = "Number too large";
+    }
+} else {
+    $ratio      = 1.618;
+    $basePoints = 11;
+    $altPoints  = 210;
+    $base       = $basePoints;
+    $alt        = $altPoints;
+}
 
-	if ( empty( $error ) ) {
-		if ( $altPoints >= 9999 ) {
-			$max = $altPoints;
-		} else {
-			$max = 9999;
-		}
+if (empty( $error )) {
+    if ($altPoints >= 9999) {
+        $max = $altPoints;
+    } else {
+        $max = 9999;
+    }
 
-		$i = $basePoints;
-		while ( $i >= 1 ) {
-			$baseScale[] = round( $i, 1 );
-			$i           = $i / $ratio;
-		}
-		$baseScale = array_reverse( $baseScale );
-		$i         = $basePoints;
-		while ( $i <= $max ) {
-			$i           = $i * $ratio;
-			$baseScale[] = round( $i, 1 );
-		}
+    $i = $basePoints;
+    while ($i >= 1) {
+        $baseScale[] = $i;
+        $i           = $i / $ratio;
+    }
+    $baseScale = array_reverse($baseScale);
+    $i         = $basePoints;
+    while ($i <= $max) {
+        $i           = $i * $ratio;
+        $baseScale[] = $i;
+    }
 
-		$i = $altPoints;
-		while ( $i >= 1 ) {
-			$altScale[] = round( $i, 1 );
-			$i          = $i / $ratio;
-		}
-		$altScale = array_reverse( $altScale );
-		$i        = $altPoints;
-		while ( $i <= $max ) {
-			$i          = $i * $ratio;
-			$altScale[] = round( $i, 1 );
-		}
+    $i = $altPoints;
+    while ($i >= 1) {
+        $altScale[] = round($i, 1);
+        $i          = $i / $ratio;
+    }
+    $altScale = array_reverse($altScale);
+    $i        = $altPoints;
+    while ($i <= $max) {
+        $i          = $i * $ratio;
+        $altScale[] = $i;
+    }
 
-		$altScale  = new ArrayIterator( $altScale );
-		$baseScale = new ArrayIterator( $baseScale );
+    $altScale  = new ArrayIterator($altScale);
+    $baseScale = new ArrayIterator($baseScale);
 
-		$scale = new MultipleIterator();
-		$scale->attachIterator( $baseScale );
-		$scale->attachIterator( $altScale );
-	}
+    $scale = new MultipleIterator();
+    $scale->attachIterator($baseScale);
+    $scale->attachIterator($altScale);
+}
